c35eaeae8cb3a8250f7223f6060deff9de21d8e5
Martin-Ortiz <leo.martin178@gmail.com>
2022-05-27 15:32:51 -0500

    Se corrigio condicional, faltaba un =

--- js/arrayPalabras.js
+++ js/arrayPalabras.js
@@ -1,6 +1,6 @@
 palabras = JSON.parse(localStorage.getItem('myArray'));
 
-if(palabras=null){
+if(palabras==null){
     // El arreglo:
     var palabras = ["HOLA", "ALURA", "ORACLE", "MEXICO", "TREN", "RETO"];
     localStorage.setItem('myArray', JSON.stringify(palabras));
